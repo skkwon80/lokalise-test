f8a94b017108b37a80cd25b628b1153fff3e8e28
eddy <eddy.kwon@legendaries.team>
2023-02-03 17:19:57 +0900

    Init project

---
+++ src/main/kotlin/com/example/lokalisetest/LokaliseTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.lokalisetest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LokaliseTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<LokaliseTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/lokalisetest/LokaliseTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.lokalisetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LokaliseTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
