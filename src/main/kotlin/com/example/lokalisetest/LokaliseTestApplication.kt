02fcffbc175b0202599493b28811735cc1fefd83
eddy <eddy.kwon@legendaries.team>
2023-02-03 18:00:31 +0900

    메시지 등록

--- src/main/kotlin/com/example/lokalisetest/LokaliseTestApplication.kt
+++ src/main/kotlin/com/example/lokalisetest/LokaliseTestApplication.kt
@@ -2,10 +2,26 @@ package com.example.lokalisetest
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.MessageSource
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @SpringBootApplication
 class LokaliseTestApplication
 
 fun main(args: Array<String>) {
     runApplication<LokaliseTestApplication>(*args)
+}
+
+@RestController
+class Test(
+    val messageSource: MessageSource
+) {
+    @GetMapping("/test")
+    fun test(
+        locale: Locale
+    ): String {
+        return messageSource.getMessage("message", null, Locale.of("ko"))
+    }
 }
